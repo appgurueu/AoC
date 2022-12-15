b7949eee0cf5eee92197c8c62dafed5514c8772a
Lars Mueller <appgurulars@gmx.de>
2022-12-15 12:40:30 +0100

    Optimize day 15 part 2

--- 22/src/15/2.lua
+++ 22/src/15/2.lua
@@ -19,49 +19,36 @@ for line in ... do
 	table.insert(sensors, {x = sx, y = sy, range = range})
 end
 
-local function intersect(a_min, a_max, b_min, b_max)
-	return a_min <= b_max and a_max >= b_min
-end
+return coroutine.wrap(function()
+	-- Note: Positions being tried multiple times doesn't matter.
+	local function try_pos(x, y)
+		for _, sensor in ipairs(sensors) do
+			if distance(sensor.x, sensor.y, x, y) <= sensor.range then
+				return -- in range
+			end
+		end
+		coroutine.yield(y + max * x)
+	end
 
--- ideally we'd be intersecting the diamond shapes produced by the Manhattan distance
--- (which are just rects if rotated 45°) instead
-for row_y = 0, max do
-	local intervals = {} -- ideally this would use a sorted set data structure
 	for _, sensor in ipairs(sensors) do
-		local dist = math.abs(sensor.y - row_y)
-		if dist <= sensor.range then
-			local dist_left = sensor.range - dist
-			local min_x, max_x = sensor.x - dist_left, sensor.x + dist_left
-			local new_intervals = {}
-			for _, interval in ipairs(intervals) do
-				if intersect(min_x, max_x, interval[1], interval[2]) then
-					min_x = math.min(min_x, interval[1])
-					max_x = math.max(max_x, interval[2])
-				else
-					table.insert(new_intervals, interval)
-				end
-			end
-			table.insert(new_intervals, {min_x, max_x})
-			intervals = new_intervals
+		-- Iterate the outline of just barely out-of-range positions
+		local sx, sy, sr = sensor.x, sensor.y, sensor.range
+		for i = 0, sr + 1 do
+			try_pos(sx + sr + 1 - i, sy + i)
+			try_pos(sx + sr + 1 - i, sy - i)
+			try_pos(sx + i, sy + sr + 1 - i)
+			try_pos(sx - i, sy + sr + 1 - i)
 		end
 	end
-	table.sort(intervals, function(a, b)
-		return a[1] < b[1]
-	end)
-	local x
-	if intervals[1][1] == 1 then
-		x = 0
-	elseif intervals[#intervals][2] == max - 1 then
-		x = max
-	else
-		for i = 1, #intervals - 1 do
-			if intervals[i][2] + 2 == intervals[i+1][1] then -- gap of exactly one
-				x = intervals[i][2] + 1
-				break
-			end
-		end
+
+	-- Try borders
+	for i = 0, max do
+		try_pos(0, i)
+		try_pos(i, 0)
+		try_pos(max, i)
+		try_pos(i, max)
 	end
-	if x then
-		return row_y + max * x
-	end
-end
+
+	error"no position found"
+end)()
+
